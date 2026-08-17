d0326e54862360896134a1c77dd0d021e815d0de
M <38927629+YZmarkS@users.noreply.github.com>
2026-08-17 02:22:05 -0400

    Help build

--- build.el
+++ build.el
@@ -1,3 +1,16 @@
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
 (require 'ox-publish)
 
 ;; Define the publishing project
@@ -9,7 +22,9 @@
              :publishing-directory "./public_html"
              :publishing-function 'org-html-publish-to-html
 	     :with-author nil
-	     :with-toc nil)
+	     :with-toc nil
+	     :section-numbers nil
+	     :time-stamp-file nil)
        (list "static"
 	     :recursive t
 	     :base-directory "./Content"
@@ -19,7 +34,14 @@
 
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
-      org-html-head "<link rel=\"stylesheet\" href=\"/style.css\">")
+      org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"/style.css\">"
+      org-html-preamble
+      (concat "<div id=\"main-header\">"
+	      "<a href=\"/index.html\">Home</a>"
+	      "<a href=\"/Notes/index.html\">Notes</a>"
+	      "<a href=\"/Development_Blog/index.html\">Development Blog</a>"
+	      "</div>"))
 
 (org-publish-all t)
 
